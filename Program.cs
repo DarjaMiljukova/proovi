105a9c14e0c6b7642526b8a4818ea11b4b339db0
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-09-05 14:14:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using proovi;
+//public class StudentMain
+//{
+//    public static void Main()
+//    {
+//        Student student = new Student("John", 17, "London");
+
+//        //Console.WriteLine(student.Name);
+//        //Console.WriteLine(student.Age);
+//        //Console.WriteLine(student.GetCity());
+//        student.ShowInfo();
+//    }
+
+//    DateTime dateTime= DateTime.Now;
+//}
+
+
+public class Program
+{
+    public static void Main()
+    {
+       List<Message> list = new List<Message>();
+        Random r= new Random();
+        Message m = new Message();
+        int MCount=r.Next(1,10);
+        for (int i = 0; i < MCount; i++) //начало цикла, запрос на
+        {
+            Console.WriteLine("Sms: ");
+            string c = Console.ReadLine();
+            Console.WriteLine("Autor: ");
+            string a = Console.ReadLine();
+            int t = -1*r.Next(1,1000000);
+            DateTime dhm= DateTime.Now.Date.AddSeconds(t);
+            m = new Message(c, a, dhm);
+            for (int j = 0; j<r.Next(1,1000);j++)
+            {
+                m.AddLike();
+            }
+            list.Add(m);
+            m.ShowMessageInfo();
+        }
+        Console.WriteLine(m.GetPopularityInfo(list));
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proovi
+{
+    //    public class Student
+    //    {
+    //        public string Name { get; }
+    //        public int Age;
+    //        private readonly string _city;
+    //        public string status;
+    //        public int dateTime
+    //        {
+
+    //        }
+
+    //        public Student(string name, int age, string city)
+    //        {
+    //            Name = name;
+    //            Age = age;
+    //            _city = city;
+    //        }
+
+    //        public string Status
+    //        { 
+    //            get { return status; } 
+    //            set 
+    //            { 
+    //                status = value; 
+    //                if (Age<18)                
+    //                status = "alaealine";
+    //                else status = "täiskasvanud";
+
+    //            }
+    //        }
+
+    //        public string GetCity()
+    //        {
+    //            return _city;
+    //        }
+    //        public void ShowInfo()
+    //        {
+    //            Console.WriteLine(Name);
+    //            Console.WriteLine(Age);
+    //            Console.WriteLine(GetCity());
+    //            Console.WriteLine(Status);
+    //        }
+
+    //    }
+
+    public class Message
+    {
+        private readonly string _content;
+        private readonly string _author;
+        private readonly DateTime _time;
+        private int _likes;
+
+
+        public Message() { }
+
+        public Message(string content, string author, DateTime time)
+        {
+            _content = content;
+            _author = author;
+            _time = time;
+        }
+
+        public int Likes
+        {
+            get => _likes;
+            set
+            {
+                if (value >= 0)
+                {
+                    _likes = value;
+                }
+            }
+        }
+
+        public DateTime Time => _time;
+        public string Author => _author;
+        public string Content => _content;
+
+        public void AddLike()
+        {
+            _likes++;
+        }
+
+        public double GetPopularity()
+        {
+            TimeSpan elapsed = DateTime.Now - _time;
+            if (elapsed.TotalSeconds == 0)
+            {
+                return _likes;
+            }
+            return _likes / elapsed.TotalSeconds;
+        }
+
+        public void ShowMessageInfo()
+        {
+            Console.WriteLine(Content);
+            Console.WriteLine(Author);
+            Console.WriteLine(Time);
+        }
+
+
+
+
+        public string GetPopularityInfo(double esimene, double teine)
+        {
+            string rezult = " ";
+            if (esimene > teine)
+            {
+                rezult = "Esimene sõnum on populaarsem kui teine:";
+            }
+            else if (teine > esimene)
+            {
+                rezult = "Teine sõnum on populaarsem kui esimene";
+            }
+            return rezult;
+        }
+
+        public static string GetMostPopularMessage(List<Message> messages)
+        {
+            string result = " ";
+            double maxPopularity = 0;
+            foreach (var message in messages)
+            {
+                double popularity = message.GetPopularity();
+                if (popularity > maxPopularity)
+                {
+                    maxPopularity = popularity;
+                    result = $"{message.Content} on kõige populaarsem sõnum, seda kirjutas {message.Author}";
+                }
+            }
+            return result;
+        }
+    }
+}
